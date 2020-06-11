224cc719c7c965d2c381f5b243f902f450a8142f
Chris Attaway <chris.d.attaway@gmail.com>
2020-06-10 21:49:36 -0500

    Added bus stop controller

---
+++ BusStop/Controllers/BusStopController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using BusStop.Business.BusinessLogic;
+using BusStop.Business.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BusStop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BusStopController : ControllerBase
+    {
+        private IBusStopTimeBusinessLogic BusStopTimeBusinessLogic { get; set; }
+
+        public BusStopController(IBusStopTimeBusinessLogic busStopTimeBusinessLogic)
+        {
+            this.BusStopTimeBusinessLogic = busStopTimeBusinessLogic;
+        }
+
+        [HttpGet("{busStopId}")]
+        public IActionResult GetNextArrivals(int busStopId)
+        {
+            // hard-coding the number of arrival times for now
+            var result = this.BusStopTimeBusinessLogic.GetNextStopTimes(busStopId, 2);
+
+            if (result.IsSuccessful == true)
+            {
+                return this.Ok(result.Result);
+            }
+
+            switch (result.ResultCode)
+            {
+                case BusStopTimeResultCode.InvalidStop:
+                    return this.NotFound(result.ResultMessage);
+                case BusStopTimeResultCode.UnknownError:
+                default:
+                    return this.StatusCode((int)HttpStatusCode.InternalServerError, result.ResultMessage);
+            }
+        }
+    }
+}
