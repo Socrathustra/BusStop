d61c0fe0c3d91929fde4191c01258e904d8bd3e7
Chris Attaway <chris.d.attaway@gmail.com>
2020-06-10 19:49:26 -0500

    Added tests for service and business logic

---
+++ BusStop.Business.Test/BusStopTimeBusinessLogicShould.cs
@@ -0,0 +1,71 @@
+using BusStop.Business.BusinessLogic;
+using BusStop.Business.Helpers;
+using BusStop.Business.Services;
+using BusStop.Core;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace BusStop.Business.Test
+{
+    public class BusStopTimeBusinessLogicShould : TestingBase<IBusStopTimeBusinessLogic>
+    {
+        [Theory]
+        [InlineData(1, 2)]
+        [InlineData(1, 3)]
+        [InlineData(1, 100)]
+        [InlineData(5, 3)]
+        public void ReturnAsManyTimesAsRequested(int busStopId, int numberOfTimes)
+        {
+            // arrange
+            var sut = this.GetSut();
+            // act
+            var actual = sut.GetNextStopTimes(busStopId, numberOfTimes);
+            // assert
+            foreach (var route in actual.Result)
+            {
+                // should only have a single bus stop
+                Assert.Equal(1, route.BusStops.Count);
+                foreach (var stop in route.BusStops)
+                {
+                    // Should return as many arrival times as requested
+                    Assert.Equal(numberOfTimes, stop.ArrivalTimes.Count);
+                }
+            }
+        }
+
+        [Fact]
+        public void IndicateAnInvalidStopWhenThereAreNoResults()
+        {
+            // arrange
+            var services = FakeStartup.GetDefaultServices();
+            var subDataService = Substitute.For<IRouteStopDataService>();
+            subDataService.GetByBusStop(Arg.Any<int>(), Arg.Any<int>()).Returns(new List<RouteStop>());
+            services.AddScoped<IRouteStopDataService>(x => subDataService);
+            var sut = this.GetSut(services);
+            // act
+            var actual = sut.GetNextStopTimes(1, 1);
+            // assert
+            Assert.Equal(BusStopTimeResultCode.InvalidStop, actual.ResultCode);
+        }
+
+        [Fact]
+        public void IndicateAnUnknownErrorDuringUnhandledExceptions()
+        {
+            // arrange
+            var services = FakeStartup.GetDefaultServices();
+            var subDataService = Substitute.For<IRouteStopDataService>();
+            subDataService.GetByBusStop(Arg.Any<int>(), Arg.Any<int>()).Throws(new Exception("test"));
+            services.AddScoped<IRouteStopDataService>(x => subDataService);
+            var sut = this.GetSut(services);
+            // act
+            var actual = sut.GetNextStopTimes(1, 1);
+            // assert
+            Assert.Equal(BusStopTimeResultCode.UnknownError, actual.ResultCode);
+        }
+    }
+}
---
+++ BusStop.Business.Test/RouteStopDataServiceShould.cs
@@ -0,0 +1,29 @@
+﻿using BusStop.Business.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace BusStop.Business.Test
+{
+    public class RouteStopDataServiceShould : TestingBase<IRouteStopDataService>
+    {
+        [Theory]
+        [InlineData(1)]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(100)]
+        public void ReturnListOfRouteStopsForABusStop(int numberOfTimes)
+        {
+            // arrange
+            // todo: this would break after adding a database layer, but it could be fixed by adding a mock database to the default test service collection
+            var sut = this.GetSut();
+            // act
+            var actual = sut.GetByBusStop(1, numberOfTimes);
+            // assert
+            Assert.True(actual.All(x => x.ArrivalTimes.Count == numberOfTimes));
+            Assert.True(actual.All(x => x.BusStopId == 1));
+        }
+    }
+}
