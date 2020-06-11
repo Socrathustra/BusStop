02ce2914a9366ef1736331bc6db930e17c3be750
Chris Attaway <chris.d.attaway@gmail.com>
2020-06-10 19:48:50 -0500

    Registered dependencies in Startup

--- BusStop/Startup.cs
+++ BusStop/Startup.cs
@@ -1,10 +1,13 @@
+using BusStop.Business.BusinessLogic;
+using BusStop.Business.Helpers;
+using BusStop.Business.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace BusStop
 {
@@ -17,20 +20,30 @@ namespace BusStop
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+
+        // This method gets called by the runtime. Use this method to add services to the container. NOTE: cannot have multiple overloads
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
+            services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            this.AddBusinessDependencies(services);
+        }
+
+        // Allows the application's business dependencies to be added to a service collection without having to add the dependencies necessary to run the entire application
+        public void AddBusinessDependencies(IServiceCollection services)
+        {
+            services.AddScoped<IBusStopTimeBusinessLogic, BusStopTimeBusinessLogic>();
+            services.AddScoped<IRouteStopDataService, HardCodedRouteStopDataService>();
+            services.AddScoped<IDateTimeNow, DateTimeNow>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -39,18 +52,24 @@ namespace BusStop
             else
             {
                 app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseSpaStaticFiles();
-
-            app.UseMvc(routes =>
+            if (!env.IsDevelopment())
             {
-                routes.MapRoute(
+                app.UseSpaStaticFiles();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
                     name: "default",
-                    template: "{controller}/{action=Index}/{id?}");
+                    pattern: "{controller}/{action=Index}/{id?}");
             });
 
             app.UseSpa(spa =>
